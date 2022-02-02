1126dfe7ca0fd9d986c1046caa81153571460376
Bisrat Walle <87634885+bisrat-walle@users.noreply.github.com>
2022-02-02 18:53:36 +0300

    Time: 192 ms (5.13%), Space: 13.8 MB (99.95%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        num1 = ""
+        num2 = ""
+        current = l1
+        while current is not None:
+            num1 += str(current.val)
+            current = current.next
+        
+        current = l2
+        while current is not None:
+            num2 += str(current.val)
+            current = current.next
+        
+        num = str(int("".join(reversed(num1))) + int("".join(reversed(num2))))
+        
+        l = ListNode()
+        l.val = int(num[-1])
+        current = l
+        for i in reversed(num[:-1]):
+        # for i in reversed(num):
+            # current.val = int(i)
+            # new_node = ListNode()
+            # current.next = new_node
+            # if current.next is not None:
+            #     current = new_node
+            new_node = ListNode(int(i))
+            current.next = new_node
+            current = current.next
+        return l
