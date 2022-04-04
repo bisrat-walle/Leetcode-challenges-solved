39efbcf1f93eaeb4d3be1c7193ab4cd111a9e39e
Bisrat Walle <87634885+bisrat-walle@users.noreply.github.com>
2022-04-04 22:10:14 +0300

    Time: 68 ms (75.54%), Space: 14 MB (80.52%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
+        last = cost[-1]
+        before_last = cost[-2]
+        n = len(cost)
+        
+        for i in reversed(range(n-2)):
+            res = cost[i] + min(before_last,last)
+            last = before_last
+            before_last = res
+        return min(before_last,last)
