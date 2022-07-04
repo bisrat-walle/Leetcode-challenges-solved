c12845580e7d10116accc33ccbad0fd8bcb43bcf
Bisrat Walle <87634885+bisrat-walle@users.noreply.github.com>
2022-07-04 22:22:25 +0300

    Time: 85 ms (5.32%), Space: 14.4 MB (33.92%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,14 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         an = set()
-        @lru_cache(None)
+        cache = set()
         def dfs(remaining=tuple(nums), prev=()):
+            if (remaining, prev) in cache:
+                return
             an.add(prev)
             for i in range(len(remaining)):
                 new_remaining = remaining[:i]+remaining[i+1:]
                 new_remaining = tuple(sorted(new_remaining))
                 new_prev = tuple(sorted(prev+(remaining[i],)))
                 dfs(new_remaining, new_prev)
+            cache.add((remaining, prev))
         dfs()
         return list(map(list, an))
                 
