d06b58f4333e022eb36063ac014717d8649dce46
Bisrat Walle <87634885+bisrat-walle@users.noreply.github.com>
2022-09-10 20:30:15 +0300

    Time: 6707 ms (7.20%), Space: 620.1 MB (5.01%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        @cache
+        def rec(index, transaction=0, hasStock=False):
+            if index == len(prices) or transaction == 2:
+                return 0
+            ans = rec(index+1, transaction, hasStock)
+            if hasStock:
+                ans = max(ans, prices[index]+rec(index+1, transaction+1, not hasStock))
+            else:
+                ans = max(ans, -prices[index]+rec(index+1, transaction, not hasStock))
+            return ans
+        return rec(0)
