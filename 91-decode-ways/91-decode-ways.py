41fdfaf639a0706539383fa944651896bad4da5e
Bisrat Walle <87634885+bisrat-walle@users.noreply.github.com>
2022-10-01 09:07:57 +0300

    Time: 39 ms (84.58%), Space: 13.8 MB (80.35%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,21 +1,12 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         mapping = dict(zip(map(str, range(1, 27)), map(lambda k:chr(k+64), range(1, 27))))
-        cache = {}
-        def rec(i):
-            if i in cache:
-                return cache[i]
-            if i >= len(s):
-                return 1
-            ans = 0
-            
-            option1 = s[i]
-            if i < len(s)-1:
-                option2 = s[i:i+2]
-                if option2 in mapping:
-                    ans += rec(i+2)
-            if option1 in mapping:
-                ans += rec(i+1)
-            cache[i] = ans
-            return cache[i]
-        return rec(0)
+        dp = [0 for i in range(len(s)+1)]
+        dp[len(s)] = 1
+        for i in reversed(range(len(s))):
+            if i != len(s)-1:
+                if s[i:i+2] in mapping:
+                    dp[i] += dp[i+2]
+            if s[i] in mapping:
+                dp[i] += dp[i+1]
+        return dp[0]
