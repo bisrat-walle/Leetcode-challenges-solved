afa6f51371335a6c547e694a0909af9df5e772cb
Bisrat Walle <87634885+bisrat-walle@users.noreply.github.com>
2022-10-02 16:57:59 +0300

    Time: 3663 ms (5.05%), Space: 14.4 MB (78.74%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -1,15 +1,25 @@
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
         mod = 10**9+7
-        @lru_cache(None)
-        def rec(current, n):
-            if current == 0 and n == 0:
-                return 1
-            if n == 0:
-                return 0
-            ans = 0
-            for i in range(1, k+1):
-                ans = (ans%mod + rec(current-i, n-1)%mod)%mod
-            return ans
+        dp = [[0 for i in range(n+1)] for j in range(target+1)]
+        dp[0][0] = 1
         
-        return rec(target, n)
+        for i in range(1, target+1):
+            for j in range(1, n+1):
+                for k in range(1, k+1):
+                    if i-k > -1:
+                        dp[i][j] = (dp[i][j]%mod + dp[i-k][j-1]%mod)%mod
+                
+# Top down        
+#         @lru_cache(None)
+#         def rec(current, n):
+#             if current == 0 and n == 0:
+#                 return 1
+#             if n == 0:
+#                 return 0
+#             ans = 0
+#             for i in range(1, k+1):
+#                 ans = (ans%mod + rec(current-i, n-1)%mod)%mod
+#             return ans
+        
+        return dp[target][n]
