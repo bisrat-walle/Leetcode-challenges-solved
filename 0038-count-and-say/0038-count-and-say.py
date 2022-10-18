8cc09294305a229acf6691c1f9c284b681bae816
Bisrat Walle <87634885+bisrat-walle@users.noreply.github.com>
2022-10-18 08:54:26 +0300

    Time: 124 ms (10.08%), Space: 14.1 MB (16.06%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,14 +1,16 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
-        if n == 1:
-            return "1"
-        res = self.countAndSay(n-1)
-        ans = []
-        cnt = 1
-        for index in range(1, len(res)+1):
-            if index == len(res) or res[index] != res[index-1]:
-                ans.extend([cnt, res[index-1]])
-                cnt = 1
-            else:
-                cnt += 1
-        return "".join(map(str, ans))
+        # Iterative Version 
+        current = "1"
+        for i in range(1, n):
+            res = current
+            ans = []
+            cnt = 1
+            for index in range(1, len(res)+1):
+                if index == len(res) or res[index] != res[index-1]:
+                    ans.extend([cnt, res[index-1]])
+                    cnt = 1
+                else:
+                    cnt += 1
+            current = "".join(map(str, ans))
+        return current
